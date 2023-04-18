dc0d5961b1b2af9da4a117556be51774808b18b4
FreakPerry <elisei.tatarenko@gmail.com>
2023-04-18 13:51:31 +0400

    feat: prepare for publication

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,7 @@ const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
 const popupSaveButton = popupElement.querySelector('.popup__save-button');
 const inputElements = popupElement.querySelectorAll('.popup__form-input');
+const formElement = popupElement.querySelector('.popup__form');
 
 const togglePopupVisability = function () {
   popupElement.classList.toggle('popup_is-opened');
@@ -19,13 +20,13 @@ popupCloseButtonElement.addEventListener('click', togglePopupVisability);
 inputElements[0].value = profileNameElement.textContent;
 inputElements[1].value = profileAboutElement.textContent;
 
-const editInfo = function () {
+const handleFormSubmit = function (evt) {
+  evt.preventDefault();
   let newName = inputElements[0].value;
   let newAbout = inputElements[1].value;
-
   profileNameElement.textContent = newName;
   profileAboutElement.textContent = newAbout;
 };
 
-popupSaveButton.addEventListener('click', editInfo);
+popupSaveButton.addEventListener('click', handleFormSubmit);
 popupSaveButton.addEventListener('click', togglePopupVisability);
