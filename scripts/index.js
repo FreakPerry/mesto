6cabb4953da907dfaff4fd63f4ae5843d31b41ac
FreakPerry <elisei.tatarenko@gmail.com>
2023-04-19 22:18:03 +0400

    feat: correct comments

--- scripts/index.js
+++ scripts/index.js
@@ -3,30 +3,30 @@ const profileInfoElement = profileElement.querySelector('.profile__info');
 const profileNameElement = profileInfoElement.querySelector('.profile__name');
 const profileAboutElement = profileInfoElement.querySelector('.profile__about');
 
-const popupOpenButtonEditProfileElement = profileInfoElement.querySelector('.profile__edit-button');
+const editProfileButton = profileInfoElement.querySelector('.profile__edit-button');
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
 const popupSaveButton = popupElement.querySelector('.popup__save-button');
-const inputElements = popupElement.querySelectorAll('.popup__form-input');
 const formElement = popupElement.querySelector('.popup__form');
+const nameInput = document.getElementById('name-input');
+const aboutInput = document.getElementById('about-input');
+
+nameInput.value = profileNameElement.textContent;
+aboutInput.value = profileAboutElement.textContent;
 
 const togglePopupVisability = function () {
   popupElement.classList.toggle('popup_is-opened');
 };
 
-popupOpenButtonEditProfileElement.addEventListener('click', togglePopupVisability);
+editProfileButton.addEventListener('click', togglePopupVisability);
 popupCloseButtonElement.addEventListener('click', togglePopupVisability);
-
-inputElements[0].value = profileNameElement.textContent;
-inputElements[1].value = profileAboutElement.textContent;
 
 const handleFormSubmit = function (evt) {
   evt.preventDefault();
-  let newName = inputElements[0].value;
-  let newAbout = inputElements[1].value;
-  profileNameElement.textContent = newName;
-  profileAboutElement.textContent = newAbout;
+  profileNameElement.textContent = nameInput.value;
+  profileAboutElement.textContent = aboutInput.value;
+
+  togglePopupVisability();
 };
 
-popupSaveButton.addEventListener('click', handleFormSubmit);
-popupSaveButton.addEventListener('click', togglePopupVisability);
+formElement.addEventListener('submit', handleFormSubmit);
