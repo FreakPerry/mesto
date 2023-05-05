941398b153cb4fe79780475c5b558a9315ae7846
FreakPerry <elisei.tatarenko@gmail.com>
2023-05-05 13:03:04 +0400

    feat: add popup for adding cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,104 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+// profile info
+
 const profileElement = document.querySelector('.profile');
 const profileInfoElement = profileElement.querySelector('.profile__info');
 const profileNameElement = profileInfoElement.querySelector('.profile__name');
 const profileAboutElement = profileInfoElement.querySelector('.profile__about');
 
+// profile buttons
+
 const editProfileButton = profileInfoElement.querySelector('.profile__edit-button');
-const popupElement = document.querySelector('.popup');
-const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
-const popupSaveButton = popupElement.querySelector('.popup__save-button');
-const formElement = popupElement.querySelector('.popup__form');
+const addCardButton = profileElement.querySelector('.profile__add-button');
+
+// popups
+
+const popupEdit = document.querySelector('#popup-edit-profile');
+const popupAdd = document.querySelector('#popup-add-card');
+
+// edit popup elements
+
+const editPopupCloseButton = popupEdit.querySelector('.popup__close-button');
+const popupSaveButton = popupEdit.querySelector('.popup__save-button');
+const formElement = popupEdit.querySelector('.popup__form');
 const nameInput = document.getElementById('name-input');
 const aboutInput = document.getElementById('about-input');
 
-const togglePopupVisability = function () {
-  nameInput.value = profileNameElement.textContent;
-  aboutInput.value = profileAboutElement.textContent;
+// add popup elements
 
-  popupElement.classList.toggle('popup_is-opened');
+const addPopupCloseButton = popupAdd.querySelector('#add-popup-close-button');
+const addPopupCreateButton = popupAdd.querySelector('.popup__save-button');
+const addCardForm = popupAdd.querySelector('.popup__form');
+const titleInput = document.getElementById('title-input');
+const imageLinkInput = document.getElementById('link-input');
+
+// cards
+
+const cards = document.querySelectorAll('.card');
+const cardTemplate = document.querySelector('.card-template');
+
+// functions
+
+const togglePopupVisability = function (popup) {
+  popup.classList.toggle('popup_is-opened');
 };
 
-editProfileButton.addEventListener('click', togglePopupVisability);
-popupCloseButtonElement.addEventListener('click', togglePopupVisability);
-
-const handleFormSubmit = function (evt) {
+function handleFormSubmit(evt) {
   evt.preventDefault();
   profileNameElement.textContent = nameInput.value;
   profileAboutElement.textContent = aboutInput.value;
 
-  togglePopupVisability();
-};
+  togglePopupVisability(popupEdit);
+}
 
 formElement.addEventListener('submit', handleFormSubmit);
+
+editProfileButton.addEventListener('click', () => {
+  nameInput.value = profileNameElement.textContent;
+  aboutInput.value = profileAboutElement.textContent;
+  togglePopupVisability(popupEdit);
+});
+
+function handleAddCardButtonClick() {
+  togglePopupVisability(popupAdd);
+}
+
+addCardButton.addEventListener('click', handleAddCardButtonClick);
+
+function handlePopupCloseButtonClick(popup) {
+  togglePopupVisability(popup);
+}
+
+editPopupCloseButton.addEventListener('click', () => {
+  handlePopupCloseButtonClick(popupEdit);
+});
+
+addPopupCloseButton.addEventListener('click', () => {
+  handlePopupCloseButtonClick(popupAdd);
+});
